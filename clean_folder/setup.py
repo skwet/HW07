72d24eafbfe5a09d44c74642a6d88f3e77eeb8b4
Bohdan Datsko <bohdandatsko5@gmail.com>
2023-04-21 20:37:16 +0300

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -4,6 +4,6 @@ setup(
     name = 'clean_folder',
     version = '0.1.0',
     packages=find_namespace_packages(),
-    entry_points ={'console_scripts': ['clean-folder = clean_folder.clean:main']},
+    entry_points ={'console_scripts': ['clean-folder = clean_folder.clean:file_sorter']},
     include_package_data=True
     )
